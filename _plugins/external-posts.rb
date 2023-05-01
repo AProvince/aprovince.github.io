df4448cc3087e389c6ce732b06823140826734e5
alex.province <alexprovince23@gmail.com>
2023-04-30 19:00:19 -0600

    Missed 2 'feed'

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -44,7 +44,7 @@ module Jekyll
 def generate(site)
       jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
       site.collections['external_feed'] = jekyll_coll
-Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@alexprovince23").entries.each do |e|
+Feedjira::Feed.fetch_and_parse("https://medium.com/@alexprovince23").entries.each do |e|
         p "Title: #{e.title}, published on Medium #{e.url} #{e}"
         title = e[:title]
         content = e[:content]
